94973db426b7073abb28d4f3df22add8ec83df8f
ItamarBarrios <berserk.stg@gmail.com>
2022-07-12 19:50:17 -0500

    Ultimos Pasos

---
+++ react/components/EmailForm.tsx
@@ -0,0 +1,111 @@
+import React, { useState } from 'react'
+import { useCssHandles } from 'vtex.css-handles'
+import { useLazyQuery, useMutation } from 'react-apollo'
+
+import FIND_EMAIL from '../graphql/getEmails.graphql'
+import UPDATE_EMAILS from '../graphql/updateEmails.graphql'
+
+
+
+type Props = {
+  setOpenPopUp: any
+}
+
+const EmailForm = ({ setOpenPopUp }: Props) => {
+
+  const CSS_HANDLES = [
+    'submit__button',
+    'message'
+  ]
+
+  const handles = useCssHandles(CSS_HANDLES)
+
+  const [email, setEmail] = useState('')
+
+  const [submitMessage, setSubmitMessage] = useState('')
+
+  const [createDocument] = useMutation(UPDATE_EMAILS, {
+    onCompleted: () =>
+      setSubmitMessage('Email successfully registered'),
+    onError: () =>
+      console.log('Try again later')
+  })
+
+  const updateEmails = () => {
+    createDocument({
+      variables: {
+        document:
+        {
+          fields: [{ key: 'email', value: email }]
+        }
+      }
+    });
+
+    setTimeout(() => { setOpenPopUp(false) }, 1000);
+
+    localStorage.setItem('neverShowAgain', 'true')
+  }
+
+  const [filterQuery] = useLazyQuery(FIND_EMAIL, {
+    onCompleted: (data) => {
+      if (data?.documents.length > 0) {
+        setSubmitMessage('This email has been registered already');
+      }
+      else {
+        updateEmails();
+      }
+    }
+  }
+  )
+
+  const checkQuery = (emailValue: string) => {
+    filterQuery({
+      variables: {
+        email: `email=${emailValue}`
+      }
+    })
+
+  }
+
+  const onSubmit = (evt: any) => {
+    evt.preventDefault();
+    checkQuery(email)
+  }
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column'
+      }}>
+      <form onSubmit={onSubmit}
+        style={{
+          display: 'flex'
+        }}>
+        <div>
+          <input onChange={(e) => { setEmail(e.target.value) }}
+            type="email"
+            name="email"
+            placeholder="Email"
+            pattern="[A-Za-z0-9._+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"
+            required />
+        </div>
+        <div>
+          <button className={handles.submit__button} >
+            Submit
+          </button>
+        </div>
+      </form>
+      <p
+        style={{
+          color: 'red',
+          fontWeight: "bold"
+        }}
+        className={handles.message}>
+        {submitMessage}
+      </p>
+    </div>
+  )
+}
+
+export default EmailForm
---
+++ react/components/FirstPurchase.tsx
@@ -0,0 +1,113 @@
+import React, { useState, useEffect } from 'react'
+import Popup from 'reactjs-popup';
+import { useCssHandles } from 'vtex.css-handles'
+
+import EmailForm from './EmailForm';
+
+
+type Props = {
+  backgroundImage: string
+  popupText: string
+}
+
+const FirstPurchase = ({ backgroundImage, popupText }: Props) => {
+
+  const CSS_HANDLES = [
+    'close__button',
+    'main__container',
+    'text',
+    'form__container'
+  ]
+  const handles = useCssHandles(CSS_HANDLES)
+
+  //popup styles
+  const contentStyle = { background: 'rgba(255,255,255,0.5)' };
+  const overlayStyle = { background: 'rgba(0,0,0,0.5)' };
+  const arrowStyle = { color: '#000' }
+
+  const [openPopUp, setOpenPopUp] = useState(false)
+
+  //Using local storage to avoid render in case the close button is clicked
+  const closedWindow = localStorage.getItem('neverShowAgain')
+
+  useEffect(() => {
+    if (closedWindow === 'true') {
+      setOpenPopUp(false)
+    } else {
+      setTimeout(() => { setOpenPopUp(true) }, 2000);
+    }
+  }, [])
+
+  const handleClose = () => {
+    setOpenPopUp(false)
+    localStorage.setItem('neverShowAgain', 'true')
+  }
+
+  return <div>
+    <Popup open={openPopUp} position="right center"
+      {...{ contentStyle, overlayStyle, arrowStyle }} >
+
+      <div
+        className={handles.main__container}
+        style={{
+          height: "500px",
+          width: "800px",
+          borderRadius: "10px",
+          backgroundImage: `url(${backgroundImage})`
+        }}>
+        <button onClick={handleClose}
+          className={handles.close__button}
+          style={{
+            position: "absolute",
+            right: "1px",
+            top: "1px",
+          }}>
+          X
+        </button>
+        <div className={handles.form__container}
+          style={{
+            position: "absolute",
+            bottom: "20px",
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+            left: "20px"
+          }}
+        >
+          <h3 className={handles.text}>
+            {popupText}
+          </h3>
+          <EmailForm setOpenPopUp={setOpenPopUp} />
+        </div>
+      </div>
+    </Popup>
+  </div>
+
+
+}
+
+FirstPurchase.schema = {
+  title: "Popup",
+  type: "object",
+  properties: {
+    backgroundImage: {
+      title: "Upload image",
+      type: 'string',
+      widget: {
+        "ui:widget": "image-uploader"
+      }
+    },
+
+    popupText: {
+      title: "popup text",
+      type: 'string',
+      widget: {
+        "ui:widget": "textarea"
+      }
+    }
+  }
+
+}
+
+export default FirstPurchase
+
